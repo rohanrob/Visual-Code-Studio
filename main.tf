72bad4bf611275c65164227d214ef08cb4c20e60
Rohan <44592416+rohanrob@users.noreply.github.com>
2021-08-28 21:09:57 -0400

    commit

--- main.tf
+++ main.tf
@@ -52,23 +52,4 @@ resource "azurerm_network_interface" "mynic" {
 resource "azurerm_windows_virtual_machine" "myVM" {
   name                = "myVM01"
   resource_group_name = azurerm_resource_group.myrg.name
-  location            = azurerm_resource_group.myrg.location
-  size                = "Standard_A1_v2"
-  admin_username      = "student"
-  admin_password      = "Pa55w.rd1234"
-  network_interface_ids = [
-    azurerm_network_interface.mynic.id,
-  ]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2016-Datacenter"
-    version   = "latest"
-  }
 }
